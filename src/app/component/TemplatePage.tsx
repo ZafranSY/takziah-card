f87d1f7e9f5a88fd82b64ce4156fc138a1a01eef
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-02-23 02:10:09 +0800

    Update TemplatePage.tsx

--- src/app/component/TemplatePage.tsx
+++ src/app/component/TemplatePage.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
-import ImageSample  from "../../../public/image.png";
+import Image from "next/image";
+import ImageSample  from "../../../public/Inalillah.svg";
+import Dummypic from "../../../public/dummypic.png";
+
 import { Amiri } from "next/font/google";
  const amiri = Amiri({
   subsets:['arabic'],
@@ -8,21 +11,25 @@ import { Amiri } from "next/font/google";
  });
 interface TemplatePageProps {
   image?: string; // Path to the image
+
   message?: string; // Optional custom message
 }
 
 const TemplatePage: React.FC<TemplatePageProps> = ({
-  image = ImageSample.src,
+  image = Dummypic.src,
   message = "In Loving Memory of Someone Special", // Default placeholder message
 }) => {
   return (
-    <div className="w-full h-full flex justify-center items-center bg-gray-100 dark:bg-gray-900 p-4">
-      <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 w-full max-w-md text-center">
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="bg-gray-800 rounded-lg shadow-lg p-6 w-full h-full max-w-md text-center">
         <div className="mb-4">
           {/* Display the image */}
-          <h1 className="text-black font-arabic text-2xl">
+          {/* <h1 className="text-black font-arabic text-2xl">
           إِنَّا لِلَّٰهِ وَإِنَّا إِلَيْهِ رَاجِعُونَ
-          </h1>
+          </h1> */}
+          <div className="flex justify-center">
+            <Image src={ImageSample} alt=" إِنَّا لِلَّٰهِ وَإِنَّا إِلَيْهِ رَاجِعُونَ" width={250}/>
+          </div>
           <img
             src={image}
             alt="Generated Takziah Card"
@@ -38,8 +45,9 @@ const TemplatePage: React.FC<TemplatePageProps> = ({
             May they rest in peace and be remembered fondly.
           </p>
         </div>
-      </div>
     </div>
+    </div>
+    
   );
 };
 
