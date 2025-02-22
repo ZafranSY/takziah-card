de17ebd1aa3dd6b7c712c49159508dfc2c26a962
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-02-22 19:08:22 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import MainPage  from "./component/mainPage";
 import Sidebar from "./component/InputPanel";
+import ThemeToggle from "./component/ThemeToggle";
+import { SiE } from "react-icons/si";
+
+
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-gray-800">  
-       <Sidebar/>
+    <div className="bg-white dark:bg-gray-800 flex h-screen">  
+    <div className="w-1/3">
+      <Sidebar/>
+    </div>
+    <div className="  w-2/3">
+      <MainPage/>
+    </div>
 
     </div>
   );
