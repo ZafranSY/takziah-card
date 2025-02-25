424023341bacec389cd556531b906ab47b41c5c6
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-02-25 23:49:52 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use state"
+import React, { useState } from "react";
+
 import Image from "next/image";
 import MainPage  from "./component/mainPage";
-import Sidebar from "./component/InputPanel";
+import InputPanel from "./component/InputPanel";
 import ThemeToggle from "./component/ThemeToggle";
 import { SiE } from "react-icons/si";
 
@@ -22,7 +25,8 @@ export default function Home() {
   return (
     <div className="bg-white dark:bg-gray-800 flex h-screen">  
     <div className="w-1/4">
-      <Sidebar/>
+    <InputPanel onDataChange={handleDataChange} />
+
     </div>
     <div className="  w-3/4 mx-auto">
       <MainPage/>
