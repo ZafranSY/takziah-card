8c8a55df800eda25b1e6de7e7a5aac51fe00fc43
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-02-27 00:06:13 +0800

    Revert "Update page.tsx"
    This reverts commit f85876b6270ef32531e67219afdab73c9a5aa37f.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,31 @@ import InputPanel from "./component/InputPanel";
 import ThemeToggle from "./component/ThemeToggle";
 import { SiE } from "react-icons/si";
 
+interface TemplateData {
+  name: string;
+  image: string;
+  from: string;
+}
 
 export default function Home() {
- return(
+  const [templateData, setTemplateData] = useState<TemplateData>({
+    name: "",
+    image: "/api/placeholder/400/400",
+    from: ""
+  });
+  const handleDataChange = (data: TemplateData) => {
+    setTemplateData(data);
+  };
+  return (
+    <div className="bg-white dark:bg-gray-800 flex h-screen">  
+    <div className="w-1/4">
+    <InputPanel onDataChange = {handleDataChange} />
 
-  <main>
-    <MainPage />
-  </main>
+    </div>
+    <div className="  w-3/4 mx-auto">
+      <MainPage/>
+    </div>
+
+    </div>
   );
 }
