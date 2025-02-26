8ab273b88e6bae016ef9d15a004a9bb498538d1f
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-02-26 18:34:47 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,12 @@ import InputPanel from "./component/InputPanel";
 import ThemeToggle from "./component/ThemeToggle";
 import { SiE } from "react-icons/si";
 
-interface TemplateData {
-  name: string;
-  image: string;
-  from: string;
-}
 
 export default function Home() {
-  const [templateData, setTemplateData] = useState<TemplateData>({
-    name: "",
-    image: "/api/placeholder/400/400",
-    from: ""
-  });
-  const handleDataChange = (data: TemplateData) => {
-    setTemplateData(data);
-  };
-  return (
-    <div className="bg-white dark:bg-gray-800 flex h-screen">  
-    <div className="w-1/4">
-    <InputPanel onDataChange = {handleDataChange} />
+ return(
 
-    </div>
-    <div className="  w-3/4 mx-auto">
-      <MainPage/>
-    </div>
-
-    </div>
+  <main>
+    <MainPage />
+  </main>
   );
 }
